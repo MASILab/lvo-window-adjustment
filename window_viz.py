8ad6c780bfa41044c34d291a9a7c27be5fc7ca7c
luyuzhe111 <yuzhe.lu@vanderbilt.edu>
2020-09-30 21:21:47 -0500

    window adjustment viz

---
+++ window_viz.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    df = pd.read_csv('csv/dataset.csv')
+    x = df['window_level_manual']
+    y = df['window_width_manual']
+    z = df['lvo_radreport']
+    plt.scatter(x, y, c=z, marker='o')
+    plt.xlabel('window_level')
+    plt.ylabel('window_width')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
